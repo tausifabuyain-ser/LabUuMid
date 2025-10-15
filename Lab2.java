517603f24a6fa4d34962004bbd63ca84a1e064a4
tausifbuyain-ser <tausif.a.buyain@gmail.com>
2025-10-15 21:22:54 +0600

    done some part of lab2

--- Lab2.java
+++ Lab2.java
@@ -1,5 +1,57 @@
+// public class Lab2{
+//     public static void main(String[] args) {
+//        double regPayHour=8.25;
+//        double overPayHour = 8.25 * 1.5;
+
+//        int TotalWork=47;
+
+//         double regPay = 40 * regPayHour;
+//        int OverTime = TotalWork - 40;
+//         double TotalPay = regPay + (OverTime * overPayHour);
+
+//         System.out.println("The total paymet :$"+ TotalPay);
+
+//     }
+// }
+
+//calculating the flim ticket prize
+
+// import java.util.Scanner;
+
+// public class Lab2 {
+//     public static void main(String[] args) {
+//         System.out.print("Enter age: ");
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+//         System.out.print("If Weekends enter 1 otherwise 0:");
+//         int Check = sc.nextInt();
+
+//         if (age < 13) {
+//             if (Check == 1) {
+//                 System.out.println("It's Weekend kiddo,Pay $120");
+
+//             } else {
+//                 System.out.println("It's not a Weekend kiddo,Pay $100");
+//             }
+//         }else if(age<65){
+//             if(Check == 1){
+//                 System.out.println("It's a weekend mate, pay $180");
+//             }else{
+//                  System.out.println("It's not a weekend mate, pay $150");
+//             }
+//         }else{
+//             if(Check == 0){
+//                 System.out.println("It's a weekend sir, pay $150");
+//             }else{
+//                 System.out.println("It's not a weekend sir, pay $130");
+//             }
+
+//         }
+//     }
+// }
+
+//find Out the cgpa
+
 public class Lab2{
-    public static void main(String[] args) {
-        System.out.println("Lab 2 is done");
-    }
+    
 }
